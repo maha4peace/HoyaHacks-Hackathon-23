d1b9fb1c28a103847769542df93101843c4bb49c
maha4peace <102927550+maha4peace@users.noreply.github.com>
2023-01-29 01:13:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function rotateProximityButton() {
-    const myButton = document.getElementById("bell-alert");
-    myButton.style.transform = 'rotate(360deg)';
-  }
+
+
+
+  const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+  const buttons = document.querySelectorAll("button");
+  
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      audio.play();
+    });
+  });
+
+  Window.alert("Got your helmet? ")
+
+const get= document.getElementById("video");
+get.play();
+
+function 
+    
+}
