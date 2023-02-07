ef9ed6cee86ff5da6e313b1eacbc4be4f3dd9730
maha4peace <102927550+maha4peace@users.noreply.github.com>
2023-02-07 17:08:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
-
-
-
-  const audio1 = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-  const audio2 = new Audio("bellalertsound.mp3");
-
-
-  
-
-
-  const proximity = document.getElementById("bell-alert");
-  
-
-  const video = document.getElementById("video");
+const video = document.getElementById("video");
   const playPauseButton = document.getElementById("streaming-button");
+
+
   
-  playPauseButton.addEventListener("click", function() {
-    audio1.play();
-    if (video.paused) {
-      video.play();
-      playPauseButton.innerHTML = "Pause";  
-    } else {
-      video.pause();
-      playPauseButton.innerHTML = "Live Stream";
-    }
+playPauseButton.addEventListener("click", () => {
+      const audio1 = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+      audio1.play();
+      if (video.paused) {
+        video.play();
+        playPauseButton.innerHTML = "Pause";
+      } else {
+        video.pause();
+        playPauseButton.innerHTML = "Live Stream";
+      }
   });
   
+
+function proximitySound() {
+  const audio2 = new Audio("bellalertsound.mp3") ;
+  const proximity = document.getElementById("bell-alert");
+  
+  proximity.addEventListener("click", () => {
+
+    audio2.play();
+
+  });
+}
+
+ proximitySound(); 
+
+  
+   
+
 
 
 
